92582ea81dfd6e521931b668f94add44c0ed1640
khet <khet30072557@gmail.com>
2026-06-28 16:12:22 +0700

    fix full bot

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ PermissionsBitField
 } = require("discord.js");
 
 const TOKEN = process.env.TOKEN;
-
-// ใส่ ID ยศตรงนี้
-const ROLE_ID = "1520398527069687901";
+const ROLE_ID = "ใส่_ROLE_ID_ตรงนี้";
 
 const client = new Client({
 intents: [
@@ -24,20 +22,41 @@ GatewayIntentBits.MessageContent
 ]
 });
 
-// เก็บข้อความของแต่ละคน
 const userMessages = new Map();
 
-client.on(Events.MessageCreate, async (message) => {
+client.once(Events.ClientReady, () => {
+console.log(`ออนไลน์: ${client.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async (message)=>{
 
 if(message.author.bot) return;
 if(!message.guild) return;
 
-// ข้ามแอดมิน
+// !verify
+if(message.content === "!verify"){
+
+const row = new ActionRowBuilder()
+.addComponents(
+new ButtonBuilder()
+.setCustomId("verify")
+.setLabel("✅ รับยศแฮกเกอร์ฝึกหัด")
+.setStyle(ButtonStyle.Success)
+);
+
+await message.channel.send({
+content:"กดปุ่มเพื่อยืนยันตัวตนเป็นเเฮกเกอร์ฝึกหัด",
+components:[row]
+});
+
+}
+
+// Anti-spam
 if(
 message.member.permissions.has(
 PermissionsBitField.Flags.Administrator
 )
-) return;
+)return;
 
 const userId = message.author.id;
 const now = Date.now();
@@ -45,17 +64,15 @@ const now = Date.now();
 let data = userMessages.get(userId);
 
 if(!data){
-data = {
-count: 1,
-lastMessage: now
-};
 
-userMessages.set(userId, data);
+userMessages.set(userId,{
+count:1,
+lastMessage:now
+});
 
 return;
 }
 
-// รีเซ็ตถ้าเกิน 5 วินาที
 if(now - data.lastMessage > 5000){
 
 data.count = 1;
@@ -67,11 +84,6 @@ return;
 data.count++;
 data.lastMessage = now;
 
-console.log(
-`${message.author.tag}: ${data.count}`
-);
-
-// เตือน
 if(data.count >= 5){
 
 await message.delete().catch(()=>{});
@@ -82,7 +94,6 @@ content:`⚠️ ${message.author} กรุณาหยุดสแปม`
 
 }
 
-// Timeout
 if(data.count >= 10){
 
 await message.member.timeout(
@@ -94,8 +105,45 @@ await message.channel.send({
 content:`🚫 ${message.author} ถูก Timeout 10 นาที`
 });
 
-data.count = 0;
+data.count=0;
 
 }
 
 });
+
+client.on(
+Events.InteractionCreate,
+async interaction=>{
+
+if(!interaction.isButton()) return;
+
+if(interaction.customId==="verify"){
+
+const role =
+interaction.guild.roles.cache.get(
+ROLE_ID
+);
+
+if(!role){
+
+return interaction.reply({
+content:"❌ ไม่พบยศ",
+ephemeral:true
+});
+
+}
+
+await interaction.member.roles.add(
+role
+);
+
+await interaction.reply({
+content:"✅ รับยศเรียบร้อย",
+ephemeral:true
+});
+
+}
+
+});
+
+client.login(TOKEN);
